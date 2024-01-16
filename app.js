66ce669c75456b9fa0a08cb50a84e1976b21553a
AlexCernas2901 <acernas1@ucol.mx>
2024-01-15 19:51:48 -0600

    CORS config

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ import taskRoutes from './src/routes/tasks.js'
 import { dbConnection } from './db.js'
 import cookieParser from 'cookie-parser'
 import { PORT } from './consts.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors({
+  origin: 'http://localhost:5173', // URL del cliente que va a consumir la API (React)
+  credentials: true
+})) // Configura los headers de CORS
 app.use(express.json()) // Parsea el body de la petición a JSON
 app.use(cookieParser()) // Parsea las cookies que vienen en la petición a JSON
 
 // Rutas
-app.use('auth', authRoutes)
+app.use('/auth', authRoutes)
 app.use('/tasks', taskRoutes)
 
 dbConnection() // Conecta con la base de datos
